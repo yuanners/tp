a82aa8e85a0c6dbbd14dea833723302e88928349
NicholasChungJunJie <84615041+NicholasChungJunJie@users.noreply.github.com>
2023-03-06 22:21:15 +0800

    Add methods for printing

--- src/main/java/utility/Ui.java
+++ src/main/java/utility/Ui.java
@@ -10,4 +10,18 @@ public class Ui {
     public void printUserInput() {
         System.out.println("Please enter something: ");
     }
+
+    /**
+     * Prints string to user and moves the cursor to a new line.
+     */
+    public void println(Object string) {
+        System.out.println(string);
+    }
+
+    /**
+     * Prints string to user without moving the cursor to a new line.
+     */
+    public void print(Object string) {
+        System.out.print(string);
+    }
 }
