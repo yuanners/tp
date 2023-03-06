bfcea3bddf55a35a7407b2c975f117121a3f15a4
yuanners <beetreeear@gmail.com>
2023-03-06 22:29:13 +0800

    Add skeleton codes for validation class and custom exceptions

--- src/main/java/validation/validation.java
+++ src/main/java/validation/validation.java
@@ -1,4 +1,10 @@
 package validation;
 
+
+import app.Command;
+
 public class validation {
+    public void validateArgument(Command arg) throws invalidArgumentException{
+        // try and catch block
+    }
 }
