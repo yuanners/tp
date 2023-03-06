9a9ed82ff898aeaf64457200f0240ccef50985e8
yuanners <beetreeear@gmail.com>
2023-03-06 23:33:25 +0800

    Add constructor and methods for validation class to handle null, special character and number format exceptions

--- src/main/java/validation/validation.java
+++ src/main/java/validation/validation.java
@@ -4,7 +4,24 @@ package validation;
 import app.Command;
 
 public class validation {
-    public void validateArgument(Command arg) throws invalidArgumentException{
-        // try and catch block
+    public validation () {
+
+    }
+
+    //handles the common input validators
+    public void validateArgument (Command arg) throws invalidArgumentException {
+        if (arg.getUserInput () == null) {
+            throw new invalidArgumentException (UI.NULL_ERROR);
+        } else if (arg.getUserInput ().contains (":") || arg.getUserInput ().contains (";")) {
+            throw new invalidArgumentException (UI.ERROR_MESSAGE);
+        }
+    }
+
+    public void validateInteger (String input) throws invalidArgumentException {
+        try {
+            Integer.parseInt (input);
+        } catch (NumberFormatException n) {
+            throw new invalidArgumentException (UI.INTEGER_ERROR);
+        }
     }
 }
