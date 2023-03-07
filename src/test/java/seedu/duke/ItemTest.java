b104de366882a3c19d0eba11ac81c816e1c3e554
NicholasChungJunJie <84615041+NicholasChungJunJie@users.noreply.github.com>
2023-03-07 14:19:39 +0800

    Add JUnit test for "additem" feature

---
+++ src/test/java/seedu/duke/ItemTest.java
@@ -0,0 +1,45 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import app.Command;
+import app.MoneyGoWhere;
+import org.junit.jupiter.api.Test;
+import utility.Ui;
+
+import java.util.Scanner;
+
+class ItemTest {
+
+    public void sampleTest() {
+        assertTrue(true);
+    }
+
+    public void runTest(String input, MoneyGoWhere moneyGoWhere) {
+
+        Ui ui = new Ui();
+
+
+        ui.printUserInput();
+        String userInput = input;
+
+
+
+        Command command = new Command(userInput);
+        moneyGoWhere.handleCommand(command);
+
+    }
+
+    @Test
+    public void itemTest() {
+        MoneyGoWhere moneyGoWhere = new MoneyGoWhere();
+        runTest("additem -p 2.50 -n chicken rice", moneyGoWhere);
+
+        assertEquals("chicken rice" , moneyGoWhere.items.getItems().get(moneyGoWhere.items.getItems().size() - 1).getName() );
+        assertEquals(2.5 , moneyGoWhere.items.getItems().get(moneyGoWhere.items.getItems().size() - 1).getPrice() );
+    }
+
+
+
+}
