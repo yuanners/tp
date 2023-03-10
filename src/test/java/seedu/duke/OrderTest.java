009ed9eb883e04f05ae13fb58d6002c27852d30a
sansders <e0689644@u.nus.edu>
2023-03-11 00:02:04 +0800

    Remove comments

--- src/test/java/seedu/duke/OrderTest.java
+++ src/test/java/seedu/duke/OrderTest.java
@@ -1,17 +1,12 @@
 package seedu.duke;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import app.Command;
 import app.MoneyGoWhere;
-//import order.Order;
-//import order.OrderEntry;
 import org.junit.jupiter.api.Test;
 import utility.Ui;
 import exception.InvalidArgumentException;
-
-import java.util.ArrayList;
 
 class OrderTest {
 
@@ -32,13 +27,6 @@ class OrderTest {
 
     @Test
     public void orderTest() {
-//        MoneyGoWhere moneyGoWhere = new MoneyGoWhere();
-//        runTest("additem -p 2.50 -n chicken rice", moneyGoWhere);
-//        runTest("addorder -i 0 -q 3", moneyGoWhere);
-//        ArrayList<Order> testList = moneyGoWhere.orderList.getOrderList();
-//        ArrayList<OrderEntry> testEntries = testList.get(testList.size() - 1).getOrderEntries();
-//        assertEquals("chicken rice", testEntries.get(testEntries.size() - 1).getItem().getName());
-//        assertEquals(3, testEntries.get(testEntries.size() - 1).getQuantity());
     }
 
 }
