15f1b19d9c1d4b18b57aa4364bfbb1a5aee5b8d8
nigelhao <nigel.zch@gmail.com>
2023-03-11 05:09:05 +0800

    Update dependency version

--- src/main/java/order/OrderList.java
+++ src/main/java/order/OrderList.java
@@ -1,6 +1,7 @@
 package order;
 
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 import utility.Store;
@@ -22,13 +23,10 @@ public class OrderList {
         try {
             this.orders = store.load(type);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("File does not exist. Initializing new list");
             this.orders = new ArrayList<>();
-        } catch (JsonParseException e) {
-            System.out.println(e.getMessage());
-            this.orders = new ArrayList<>();
-        } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
+        } catch (JsonParseException | NumberFormatException e) {
+            System.out.println("Data file corrupted. Do you want to initialize new list? [Y]es or [N]o");
             this.orders = new ArrayList<>();
         }
     }
