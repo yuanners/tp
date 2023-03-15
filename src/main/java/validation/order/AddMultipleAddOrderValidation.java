9b8056cf2d87e8e388b6218098ad9c5a7c11b6fb
yuanners <beetreeear@gmail.com>
2023-03-15 22:47:46 +0800

    Update addMultipleOrderValidation according to new input format

--- src/main/java/validation/order/AddMultipleAddOrderValidation.java
+++ src/main/java/validation/order/AddMultipleAddOrderValidation.java
@@ -15,39 +15,30 @@ public class AddMultipleAddOrderValidation extends AddOrderValidation {
 
     public void validateAddMultipleOrder(Command arg) throws OrderException {
         String input = arg.getUserInput();
-        String regex = "^addorder\\s+-I\\s+\\[(\\d+\\s+\\d+)(,\\s+\\d+\\s+\\d+)*\\]$";
+        String regex = "^addorder\\s+-I\\s+\\[((\\d+:\\d+)(,\\s*(\\d+:\\d+))*)?\\]$";
 
         if(input.matches(regex)) {
-            String[] orderPairs = input.replaceAll("[^\\d\\s,]", "").split(",\\s+");
+            String orderPairsString = input.replaceAll("[^\\d\\s,:]", "");
+            String[] orderPairs;
+            if(orderPairsString.contains(",")) {
+                orderPairs = orderPairsString.split(",\\s*");
+            } else {
+                orderPairs = new String[]{orderPairsString};
+            }
 
             for(String order : orderPairs) {
-                String[] numbers = order.trim().split("\\s+");
-
-                if(isInteger(numbers[0]) && isInteger(numbers[1])) {
+                String[] numbers = order.trim().split(":");
+                if(numbers.length == 2 && isInteger(numbers[0]) && isInteger(numbers[1])) {
                     String itemIndex = numbers[0];
                     if(!(isValidIndex(itemIndex, items))) {
                         throw new OrderException(ui.getInvalidIndex());
                     }
-
                 } else {
                     throw new OrderException(ui.getInvalidMultipleOrderInteger());
                 }
             }
         } else {
-            regex = "^addorder\\s+-I\\s+\\[(\\d+\\s+\\d+)\\]$";
-            if(input.matches(regex)) {
-                String[] numbers = input.replaceAll("[^\\d\\s]", "").split("\\s+");
-                if(isInteger(numbers[0]) && isInteger(numbers[1])) {
-                    String itemIndex = numbers[0];
-                    if(!(isValidIndex(itemIndex, items))) {
-                        throw new OrderException(ui.getInvalidIndex());
-                    }
-                } else {
-                    throw new OrderException(ui.getInvalidMultipleOrderInteger());
-                }
-            } else {
-                throw new OrderException(ui.getInvalidMultipleOrderFormat());
-            }
+            throw new OrderException(ui.getInvalidMultipleOrderFormat());
         }
     }
 
